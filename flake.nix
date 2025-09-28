42e66a032a48433ac650d6425de456a110e71c9e
Ivan Johnson <git@IvanJohnson.net>
2025-09-27 23:55:43 -0400

    Create empty rust+nix project

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+	description = "driver";
+
+	inputs.nixpkgs.url = "nixpkgs/nixos-25.05-small";
+
+	outputs =
+		{ self, nixpkgs }:
+		let
+			pkgs = import nixpkgs { system = "x86_64-linux"; };
+			rustPlatform = pkgs.rustPlatform;
+			driver = rustPlatform.buildRustPackage {
+				pname = "driver";
+
+				version = "0.1.0";
+
+				src = ./.;
+
+				buildInputs = [ pkgs.makeWrapper ];
+
+				checkFlags = [
+					#"--skip=foo::bar::..."
+				];
+
+				cargoLock.lockFile = ./Cargo.lock;
+			};
+		in
+		{
+			devShells.${pkgs.system}.default = pkgs.mkShell {
+				buildInputs = [
+					pkgs.cargo
+					pkgs.cargo-flamegraph
+					pkgs.clippy
+					pkgs.lldb
+					pkgs.rustc
+					pkgs.rustfmt
+				];
+			};
+
+			packages.${pkgs.system}.default = driver;
+
+			nixos_options =
+				{
+					config,
+					lib,
+					pkgs,
+					...
+				}:
+
+				let
+					cfg = config.programs.driver;
+				in
+				{
+					options = {
+						programs.driver = {
+							enable = lib.mkEnableOption "driver";
+						};
+					};
+
+					config = lib.mkMerge [ (lib.mkIf cfg.enable { home.packages = [ driver ]; }) ];
+				};
+		};
+}
