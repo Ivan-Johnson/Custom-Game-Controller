8ed01ed6779a28f952c84309cc06bd1c13644334
Ivan Johnson <git@IvanJohnson.net>
2025-12-14 17:08:38 -0500

    Remove distracting LED blinks

--- ArduinoIDE/test/test.ino
+++ ArduinoIDE/test/test.ino
@@ -6,13 +6,12 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
   Serial.begin(9600);
 }
 
 void loop() {
   Serial.println("Hello, World!");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
