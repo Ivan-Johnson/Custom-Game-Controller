71fb36522b4b5539baeba50c140b0c5ec8bf7106
Ivan Johnson <git@IvanJohnson.net>
2026-01-20 20:22:17 -0500

    Setup HID keyboard

--- Firmware/src/main.rs
+++ Firmware/src/main.rs
@@ -9,9 +9,20 @@ use usb_device::device::StringDescriptors;
 use usb_device::device::UsbDeviceBuilder;
 use usb_device::device::UsbVidPid;
 use usb_device::LangID;
-use usbd_hid::descriptor::MouseReport;
-use usbd_hid::descriptor::SerializedDescriptor;
+use usbd_hid::descriptor::KeyboardReport;
 use usbd_hid::hid_class::HIDClass;
+use usbd_hid::descriptor::SerializedDescriptor;
+
+// TODO short term: I should be using a custom descriptor instead of
+// a keyboard report. Examples:
+//
+// * https://github.com/twitchyliquid64/usbd-hid/blob/master/src/descriptor.rs#L61-L86
+// * https://github.com/twitchyliquid64/usbd-hid/blob/master/macros/src/lib.rs#L51-L64
+//
+// TODO long term: would it make sense to merge such a descriptor upstream?
+// Perhaps not, given that different video game controllers support different
+// keys. Then again, you could say the same thing about keyboards and
+// mice. Maybe merge many descriptors upstream? e.g. xbox, playstation, generic.
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -23,7 +34,8 @@ fn main() -> ! {
 	let usb_bus = arduino_hal::default_usb_bus_with_pll_macro!(dp);
 	let usb_bus_allocator = UsbBusAllocator::new(usb_bus);
 
-	let mut hid_class = HIDClass::new(&usb_bus_allocator, MouseReport::desc(), 1);
+	let poll_period_ms = 5;
+	let mut hid_class = HIDClass::new(&usb_bus_allocator, KeyboardReport::desc(), poll_period_ms);
 
 	let string_descriptors = StringDescriptors::new(LangID::EN_US)
 		.manufacturer("test manufacturer")
@@ -45,6 +57,9 @@ fn main() -> ! {
 	delay_ms(500);
 
 	let mut counter = 0;
+	let mut button_is_pressed = false;
+
+	// This will hold down the "A" button for a second or two, release it for a second or two, and then repeat infinitely.
 	loop {
 		counter += 1;
 		ufmt::uwriteln!(&mut serial_hw, "Cycle {}", counter).unwrap_infallible();
@@ -53,13 +68,23 @@ fn main() -> ! {
 
 		if counter % 1000 == 0 {
 			counter = 0;
+			button_is_pressed = !button_is_pressed;
+
+			let mut keycodes = [0u8; 6];
+			if button_is_pressed {
+				// Keycode obtained from:
+				// https://gist.github.com/mildsunrise/4e231346e2078f440969cdefb6d4caa3
+				//
+				// TODO: Create an enum upstream?
+				keycodes[0] = 0x04; // 'a'
+			}
+
 			hid_class
-				.push_input(&MouseReport {
-					x: 0,
-					y: 40,
-					buttons: 0,
-					pan: 0,
-					wheel: 0,
+				.push_input(&KeyboardReport {
+					keycodes,
+					leds: 0,
+					modifier: 0,
+					reserved: 0,
 				})
 				.unwrap();
 		}
